4c6646601d2a0e0db0696006358ae1aaad4c901c
THE PC <motionlab@unimate.(none)>
2014-10-23 14:52:44 -0700

    regions and other stuff

--- scripts/navigation.py
+++ scripts/navigation.py
@@ -9,6 +9,11 @@ import tf
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import time
 import pickle
+import random
+import sys
+
+
+region_radius = 1.0
 
 def sign(x):
     if x < 0:
@@ -45,11 +50,9 @@ class Navigation:
         self.pose_pub = rospy.Publisher('/move_base_simple/goal', PoseStamped)
         self.r = rospy.Rate(10)
 
-        self.pos_dict = {
-            'home' : (-2.9, 1.4, 0),
-            'rotated_home' : (-2.9, 1.4, 3.14),
-        }
+        self.pos_dict = { }
 
+        self.region_dict = { }
 
     def get_odom(self):
         (trans, rot) = self.tf_listener.lookupTransform(self.base_frame,
@@ -62,6 +65,7 @@ class Navigation:
             self.r.sleep()
 
     def goto(self, coords):
+        print "going to:", coords
         p = get_pose_stamped(*coords)
         self.pose_pub.publish(p)
 
@@ -76,31 +80,79 @@ class Navigation:
         s = raw_input()
 
         tokens = s.split()
-        if len(tokens) != 2:
-            return
 
-        command, arg = tokens
-        if command == 'goto':
-            if not arg in self.pos_dict:
-                print 'no such position:', arg
+        command = tokens[0]
+
+        if command == 'goto_pos' or command == 'goto':
+            if not tokens[1] in self.pos_dict:
+                print 'no such position:', tokens[1]
                 return
-            self.goto(self.pos_dict[arg])
+            self.goto(self.pos_dict[tokens[1]])
 
-        elif command == 'save':
+        elif command == 'goto_region':
+            if not tokens[1] in self.region_dict:
+                print 'no such region:', tokens[1]
+                return
+            (x, y) = self.region_dict[tokens[1]]
+            t = 2*math.pi*random.random()
+            u = random.random() + random.random()
+            r = 2-u if u > 1 else u
+            (randomX, randomY) = [region_radius*r*math.cos(t), region_radius*r*math.sin(t)]
+
+            self.goto([x + randomX, y + randomY, 0])
+
+
+        elif command == 'save_pos':
             current_pos = self.get_odom()
-            print "saving", current_pos, "as", arg
-            self.pos_dict[arg] = current_pos
+            print "saving", current_pos, "as", tokens[1]
+            self.pos_dict[tokens[1]] = current_pos
 
-        elif command == 'print':
-            print (arg + ":"), self.pos_dict.get(arg)
+        elif command == 'save_region':
+            (x,y,_) = self.get_odom()
+            self.region_dict[tokens[1]] = (x,y)
+
+        elif command == 'del_pos':
+            if tokens[1] in self.pos_dict:
+                del self.pos_dict[tokens[1]]
+
+        elif command == 'del_region':
+            if tokens[1] in self.region_dict:
+                del self.region_dict[tokens[1]]
+
+        elif command == 'list_poses':
+            print self.pos_dict.keys()
+
+        elif command == 'list_regions':
+            print self.region_dict.keys()
+
+        elif command == 'print_pos':
+            print (tokens[1] + ":"), self.pos_dict.get(tokens[1])
+
+        elif command == 'print_region':
+            print (tokens[1] + ":"), self.region_dict.get(tokens[1])
 
         elif command == 'save_to':
-            with open(arg, 'w') as f:
-                pickle.dump(self.pos_dict, f)
+            with open(tokens[1], 'w') as f:
+                pickle.dump((self.pos_dict, self.region_dict), f)
 
         elif command == 'load_from':
-            with open(arg, 'r') as f:
-                self.pos_dict = pickle.load(f)
+            with open(tokens[1], 'r') as f:
+                self.pos_dict, self.region_dict = pickle.load(f)
+
+        elif command == 'exit' or command == 'q':
+            print 'exiting...'
+            sys.exit(0)
+
+        elif command == 'whereami':
+            point = self.get_odom()
+            x, y, _ = point
+            print point
+            containing_regions = []
+            for name, region in self.region_dict.iteritems():
+                rx, ry = region
+                if math.sqrt((x - rx) ** 2 + (y - ry) ** 2) <= region_radius:
+                    containing_regions.append(name)
+            print "in regions:", containing_regions
 
         else:
             print "command not recognized"
