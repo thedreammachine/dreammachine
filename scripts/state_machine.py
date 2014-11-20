accc6a97a6791dfcc6f2700a30d43266415f1601
THE PC <motionlab@unimate.(none)>
2014-11-20 11:41:46 -0800

    attempt to have state changes work via voice

--- scripts/state_machine.py
+++ scripts/state_machine.py
@@ -28,7 +28,15 @@ from geometry_msgs.msg import Twist, Vector3
 from std_msgs.msg import String
 from dream_machine.msg import MusicCommand
 
-class Fiesta:
+class EmptyState:
+    def execute(self):
+        pass
+    def cleanup(self):
+        pass
+    def take_action(self, action):
+        return None
+
+class Fiesta(EmptyState):
     def __init__(self, pub):
         self.pub = pub
         self.forward_speed = 0.1
@@ -38,7 +46,7 @@ class Fiesta:
         self.countdown = 0
 
     def state(self):
-        return StateMachine.FIESTA
+        return States.FIESTA
 
     def execute(self):
         if self.countdown == 0:
@@ -51,32 +59,64 @@ class Fiesta:
 
         return None
 
-    def cleanup(self):
-        pass
-
     def twist_factory(self):
         angular_speed = (random() * 2 - 1) * self.max_angular_speed
         velocity_msg = Twist(Vector3(self.forward_speed, 0.0, 0.0), Vector3(0.0, 0.0, angular_speed))
         return velocity_msg
 
-class StateMachine:
+    def take_action(self, action):
+        if action == Actions.END_FIESTA:
+            return States.START
+        else:
+            return None
+
+class Start(EmptyState):
+    def state(self):
+        return States.START
+    def take_action(self, action):
+        if action == Actions.BEGIN_FIESTA:
+            return States.FIESTA
+        else:
+            return None
+
+class Actions:
+    BEGIN_FIESTA = 'begin_fiesta'
+    END_FIESTA = 'end_fiesta'
+    ARRIVED_START_LOC = 'arrived_start'
+    BEGIN_FOLLOW = 'begin_follow'
+    END_FOLLOW = 'end_follow'
+    TARGET_LOST = 'target_lost'
+    TARGET_LOCATED = 'target_located'
+    END_SONG = 'end_song'
+    ARRIVED_GOAL_LOC = 'arrived_goal'
+    BEGIN_LOC = 'begin_loc'
+
+
+class States:
     START = 'start'
     FIESTA = 'fiesta'
     GO_HOME = 'go_home'
+    FOLLOW_TARGET = 'follow_target'
+    SEARCH_TARGET = 'search_target'
+    ENTERTAIN = 'entertain'
+    GO_LOC = 'go_loc'
 
+class StateMachine:
     def __init__(self):
         # TODO change back to START
+        print 'start init state_machine'
         self.pub = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=10)
         self.r = rospy.Rate(10.0)
 
-        self.state = self.state_factory(StateMachine.FIESTA)
+        self.state = self.state_factory(States.FIESTA)
+        print 'end init state_machine'
 
     def state_factory(self, state):
-        if state == StateMachine.START:
-            return None
-        elif state == StateMachine.FIESTA:
+        if state == States.START:
+            return Start()
+        elif state == States.FIESTA:
             return Fiesta(self.pub)
-        elif state == StateMachine.GO_HOME:
+        elif state == States.GO_HOME:
             return None
 
     def run(self):
@@ -86,87 +126,50 @@ class StateMachine:
 
     def loop(self):
         new_state = self.state.execute()
-        if new_state:
+        self.change_state(new_state)
+
+    def change_state(self, new_state):
+        if new_state and new_state != self.state.state():
             self.state.cleanup()
             self.state = self.state_factory(new_state)
 
+    def take_action(self, action):
+        if action:
+            change_state(self.state.take_action(action))
+
 class VoiceHandler:
 
-    def __init__(self):
-        rospy.on_shutdown(self.cleanup)
-        self.speed = 0.2
-        self.msg = Twist()
-
-        #publish to cmd_vel for movement and
-        #publish to music commands for music control
-        self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist)
+    def __init__(self, state_machine):
+        self.state_machine = state_machine
+        # publish to music commands for music control
         self.music_commands_pub = rospy.Publisher('music_commands', MusicCommand)
         self.voice_actions_pub = rospy.Publisher('~voice_actions', String)
         #subscribe to output for text of speech recognized
-        rospy.Subscriber('recognizer/output', String, self.speech_callback)
+        rospy.Subscriber('/recognizer/output', String, self.speech_callback)
         #start handling voice commands
         rospy.Service("~start", Empty, self.start)
         #stop handling voice commands
         rospy.Service("~stop", Empty, self.stop)
 
         #Disable self on bootup
-        self.started = False
+        # TODO change back
+        self.started = True
 
         self.music_corpus = CorpusBuilder().read_song_directory()
+        print 'done initializing vh'
 
-        r = rospy.Rate(10.0)
-        while not rospy.is_shutdown():
-            if self.started:
-                self.cmd_vel_pub.publish(self.msg)
-            r.sleep()
-        
-    def speech_callback(self, msg):
+    def speech_callback(self, msg, a):
+        print 'sc'
         rospy.loginfo(msg.data)
 
         #Only handle voice commands if started
         if self.started:
 
-
-            #Speed Commands
-            if msg.data.find("full speed") > -1:
-                if self.speed == 0.2:
-                    self.msg.linear.x = self.msg.linear.x*2
-                    self.msg.angular.z = self.msg.angular.z*2
-                    self.speed = 0.4
-                    self.voice_actions_pub.publish(VOICE_FULL)
-            if msg.data.find("half speed") > -1:
-                if self.speed == 0.4:
-                    self.msg.linear.x = self.msg.linear.x/2
-                    self.msg.angular.z = self.msg.angular.z/2
-                    self.speed = 0.2
-                    self.voice_actions_pub.publish(VOICE_HALF)
-            #Movement Commands
-            if msg.data.find("forward") > -1:    
-                self.msg.linear.x = self.speed
-                self.msg.angular.z = 0
-                self.voice_actions_pub.publish(VOICE_FORWARD)
-            elif msg.data.find("left") > -1:
-                if self.msg.linear.x != 0:
-                    if self.msg.angular.z < self.speed:
-                        self.msg.angular.z += 0.05
-                else:        
-                    self.msg.angular.z = self.speed*2
-                self.voice_actions_pub.publish(VOICE_LEFT)
-            elif msg.data.find("right") > -1:    
-                if self.msg.linear.x != 0:
-                    if self.msg.angular.z > -self.speed:
-                        self.msg.angular.z -= 0.05
-                else:        
-                    self.msg.angular.z = -self.speed*2
-                self.voice_actions_pub.publish(VOICE_RIGHT)
-            elif msg.data.find("back") > -1:
-                self.msg.linear.x = -self.speed
-                self.msg.angular.z = 0
-                self.voice_actions_pub.publish(VOICE_BACK)
-            elif msg.data.find("halt") > -1:          
-                self.msg = Twist()
-                self.voice_actions_pub.publish(VOICE_HALT)
-
+            print msg.data
+            if msg.data.find("halt") > -1:
+                state_machine.take_action(Actions.END_FIESTA)
+            if msg.data.find("fiesta") > -1:
+                state_machine.take_action(Actions.BEGIN_FIESTA)
 
             #Music Commands
             if msg.data in self.music_corpus:
@@ -194,14 +197,6 @@ class VoiceHandler:
                 self.music_commands_pub.publish(MusicCommand(MusicCommand.VOLUME_DOWN, []))
                 self.voice_actions_pub.publish(VOICE_VOLUME_DOWN)
 
-            self.cmd_vel_pub.publish(self.msg)
-
-
-    def cleanup(self):
-        # stop the robot!
-        twist = Twist()
-        self.cmd_vel_pub.publish(twist)
-
     def start(self, req):
         self.started = True
         rospy.loginfo("voice handler started")
@@ -216,4 +211,8 @@ class VoiceHandler:
 
 if __name__=="__main__":
     rospy.init_node('state_machine')
-    StateMachine().run()
+    sm = StateMachine()
+    vh = VoiceHandler(sm)
+
+    # sm.run starts the loop. put everything before this.
+    sm.run()
